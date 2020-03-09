1dbc66910256a0d19b955a04d0f2c2d01dd5d7d7
Erli Junior <erlijunior@outlook.com>
2020-03-08 23:24:47 -0300

    Adiciona animação no menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // SLIDER DEPOIMENTOS
   $(".testimonials-slider").owlCarousel({
     loop: true,
     margin: 0,
@@ -6,6 +7,7 @@ $(document).ready(function() {
     dots: true,
     items: 1
   });
+  // SLIDER CLIENTES
   $(".clients-slider").owlCarousel({
     loop: true,
     margin: 20,
@@ -18,4 +20,92 @@ $(document).ready(function() {
       }
     }
   });
+
+  /**Animação do Menu */
+  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+  const menu = document.querySelector(".js-menu");
+  const menuSpan = document.createElement("span");
+  menuSpan.classList.add("highlight");
+  menu.appendChild(menuSpan);
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      activeHighlight();
+    }, 200);
+  });
+
+  /**Define a posição e a largura do span Highlight */
+  function highlightLink(link) {
+    /* Remove a classe ativo dos links */
+    linksInternos.forEach(item => {
+      item.classList.remove("ativo");
+    });
+
+    const linkWidth = link.getBoundingClientRect().width;
+    const linkPosX = link.getBoundingClientRect().x;
+    const linkPosY = link.getBoundingClientRect().y;
+
+    menuSpan.setAttribute(
+      "style",
+      `width:${linkWidth}px; top:${linkPosY}px; left:${linkPosX}px;`
+    );
+
+    /*Adicona a classe ativo no link atual */
+    link.classList.add("ativo");
+  }
+
+  /**Ativa o Highlight */
+  function activeHighlight() {
+    const sections = document.querySelectorAll(".js-scroll");
+
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const link = document.querySelector(`.js-menu a[href^='#${section.id}']`);
+      if (sectionTop <= 0 && sectionTop >= -window.innerHeight) {
+        highlightLink(link);
+      }
+    });
+  }
+
+  window.addEventListener("scroll", activeHighlight);
+
+  function initScrollSuave() {
+    if (linksInternos.length) {
+      function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute("href");
+        const section = document.querySelector(href);
+
+        activeHighlight();
+
+        section.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      }
+
+      linksInternos.forEach(link => {
+        link.addEventListener("click", scrollToSection);
+      });
+    }
+  }
+  initScrollSuave();
+
+  function initMenuFixed() {
+    window.addEventListener("scroll", menuFixo);
+
+    function menuFixo() {
+      const header = document.querySelector(".header");
+      const navBrand = document.querySelector(".navbar-brand");
+      if (window.scrollY > 0) {
+        header.classList.add(`header_fixed`);
+        navBrand.classList.add("navbar-brand_top");
+      } else {
+        header.classList.remove(`header_fixed`);
+        navBrand.classList.remove("navbar-brand_top");
+      }
+    }
+    menuFixo();
+  }
+  initMenuFixed();
 });
